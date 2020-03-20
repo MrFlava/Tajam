2d6d68d1189bf3e0ff7cfc6a29478311ff001720
Mr.Flava <thatelitemaili33t@gmail.com>
2020-03-20 02:23:16 +0200

    Added slow scrollup animation

--- js/main.js
+++ js/main.js
@@ -12,6 +12,32 @@ jQuery(document).ready(function ($) {
         }
     });
 
+    $('.navbar-collapse').find('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 40)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+
     $('.scrollup').click(function () {
 
         $("html, body").animate({scrollTop: 0}, 1000);
